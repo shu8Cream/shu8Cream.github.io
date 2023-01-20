0af9922e24513763b201548fcd7fc6e472799a5f
shu8Cream <shu.vgage@gmail.com>
2023-01-20 22:19:37 +0900

    feat: 諸々を綺麗に

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import AppBar from '@mui/material/AppBar';
+import Button from '@mui/material/Button';
 import EscalatorIcon from '@mui/icons-material/Escalator';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import CssBaseline from '@mui/material/CssBaseline';
+import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
+// import Link from '@mui/material/Link';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { Link } from 'react-router-dom'
 import './MenuBar.css';
 
+// const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
 const theme = createTheme();
 
-const AboutMe = () => {
+const Home = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
@@ -41,24 +46,39 @@ const AboutMe = () => {
             pb: 6,
           }}
         >
-          <Container maxWidth="sm">
-            
-            
-          </Container>
-        </Box>
-        
-      </main>
-      {/* Footer */}
-      <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
-        <Typography variant="h6" align="center" gutterBottom>
-          <TwitterIcon sx={{ mr: 2 }} />
-          <GitHubIcon sx={{ mr: 2 }} />
-          <EscalatorIcon sx={{ mr: 2 }} />
-        </Typography>
+        <Container maxWidth="sm">
+          <Typography
+            component="h1"
+            variant="h2"
+            align="center"
+            color="text.primary"
+            gutterBottom
+          >
+            Welcome to shu8Cream's Portfolio 
+          </Typography>
+
+          <Stack
+            sx={{ pt: 4 }}
+            direction="row"
+            spacing={2}
+            justifyContent="center"
+          >
+            <Button variant="contained">About me</Button>
+            <Button variant="outlined">Skills</Button>
+          </Stack>
+        </Container>
       </Box>
-      {/* End footer */}
+    </main>
+    {/* Footer */}
+    <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
+      <Typography variant="h6" align="center" gutterBottom>
+        <TwitterIcon sx={{ mr: 2 }} />
+        <GitHubIcon sx={{ mr: 2 }} />
+        <EscalatorIcon sx={{ mr: 2 }} />
+      </Typography>
+    </Box>
+    {/* End footer */}
     </ThemeProvider>
   );
-}
-
-export default AboutMe;
+};
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,29 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import ReactDOM from 'react-dom/client';
 import App from './App';
-import AboutMe from './AboutMe';
-import reportWebVitals from './reportWebVitals';
-import {
-  BrowserRouter,
-  Route,
-  Switch,
-} from 'react-router-dom';
 
-
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Route path='/' exact component={App} />
-      <Route path='/about' exact component={AboutMe} />
-      {/* <Route path='/example' exact component={Example} />
-      <Route path='/post/edit/:id' exact component={PostEdit} /> */}
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />
+  </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
